fef648d67b490dc9360039b4447b14026d469a9d
Kuba Marek <blue.cube@seznam.cz>
2013-04-15 01:44:34 +0200

    Redid differentiation.

--- expressions.py
+++ expressions.py
@@ -4,29 +4,42 @@ import functools
 import operator
 import math
 
+def diff(expression, variable):
+    """ Return partial derivative wrt to given variable as an expression """
+    return _wrap_const(expression)._diff(variable)
+
+def numeric_diffs(expression):
+    """ Return a dictionary of variable: partial derivative """
+    expression = _wrap_const(expression)
+    return {var: expression._diff(var).get_value() for var in expression._variables()}
+
+def variables(expression):
+    """ Return a set of variables appearing in this expression """
+    return _wrap_const(expression)._variables()
+
 def add(*terms):
-    return _Add(*_wrap_const(*terms))
+    return _Add(*_wrap_consts(*terms))
 
 def sub(a, b):
-    return _Sub(*_wrap_const(a, b))
+    return _Sub(*_wrap_consts(a, b))
 
 def mul(*terms):
-    return _Mul(*_wrap_const(*terms))
+    return _Mul(*_wrap_consts(*terms))
 
 def div(a, b):
-    return _Div(*_wrap_const(a, b))
+    return _Div(*_wrap_consts(a, b))
 
 def sq(a):
     return pow(a, 2)
 
 def neg(a):
-    return _Sub(_Constant(0), _wrap_const(a)[0])
+    return _Sub(_Constant(0), _wrap_const(a))
 
 def sqrt(a):
     return pow(a, 1/2)
 
 def pow(a, b):
-    a = _wrap_const(a)[0]
+    a = _wrap_const(a)
     if b == 2:
         return _Sq(a)
     if b == 1/2:
@@ -39,13 +52,7 @@ def dot_product(ax, ay, bx, by):
     return ax * bx + ay * by
 
 def acos(a):
-    return _Acos(_wrap_const(a)[0])
-
-def _wrap_const(*args):
-    return [x if isinstance(x, Expr) else _Constant(x) for x in args]
-
-def _multiply(iterable):
-    return functools.reduce(operator.mul, iterable)
+    return _Acos(_wrap_const(a))
 
 
 class Expr(object):
@@ -55,20 +62,39 @@ class Expr(object):
     def get_value(self):
         raise NotImplementedError()
 
-    def get_pds(self):
+    def _variables(self):
+        ret = set()
+        for term in self._terms:
+            ret.update(term._variables())
+        return ret
+
+    def _diff(self, variable):
+        """ Return partial derivative of the expression wrt the variable as an expression """
         raise NotImplementedError()
 
     def __add__(self, other):
         return add(self, other)
 
+    def __radd__(self, other):
+        return add(other, self)
+
     def __sub__(self, other):
         return sub(self, other)
+
+    def __rsub__(self, other):
+        return sub(other, self)
 
     def __mul__(self, other):
         return mul(self, other)
 
-    def __div__(self, other):
+    def __rmul__(self, other):
+        return mul(other, self)
+
+    def __truediv__(self, other):
         return div(self, other)
+
+    def __rtruediv__(self, other):
+        return div(other, self)
 
     def __neg__(self):
         return neg(self)
@@ -76,22 +102,11 @@ class Expr(object):
     def __float__(self):
         return float(self.get_value())
 
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self._terms == other._terms
+
     def __str__(self):
         raise NotImplementedError()
-
-    def _term_values(self):
-        return (term.get_value() for term in self._terms)
-
-    def _pds_helper(self):
-        """ Return list of partial derivatives for each term, and set of variables
-        encountered in this list. """
-        pds_list = []
-        all_vars = set()
-        for term in self._terms:
-            pds = term.get_pds()
-            pds_list.append(pds)
-            all_vars.update(pds.keys())
-        return pds_list, all_vars
 
     def _str_infix_helper(self, op):
         return "(" + (" " + op + " ").join(str(term) for term in self._terms) + ")"
@@ -114,6 +129,11 @@ class Variable(Expr):
             self._name = "var" + str(self._auto_naming_counter)
             self.__class__._auto_naming_counter += 1
 
+        super().__init__()
+
+    def _variables(self):
+        return {self}
+
     def get_value(self):
         return self._value
 
@@ -123,8 +143,17 @@ class Variable(Expr):
     def update_value(self, difference):
         self._value += difference
 
-    def get_pds(self):
-        return {self: 1}
+    def _diff(self, var):
+        if var == self:
+            return _Constant(1)
+        else:
+            return _Constant(0)
+
+    def __hash__(self):
+        return id(self)
+
+    def __eq__(self, other):
+        return id(self) == id(other)
 
     def __str__(self):
         return self._name
@@ -133,30 +162,26 @@ class Variable(Expr):
 class _Constant(Expr):
     def __init__(self, value):
         self._value = value
+        super().__init__()
 
     def get_value(self):
         return self._value
 
-    def get_pds(self):
-        return {}
+    def _diff(self, var):
+        return _Constant(0)
 
     def __str__(self):
         return str(self.get_value())
 
 class _Add(Expr):
     def __init__(self, *terms):
-        self._terms = terms
+        super().__init__(*terms)
 
     def get_value(self):
         return math.fsum(x.get_value() for x in self._terms)
 
-    def get_pds(self):
-        ret = {}
-        for term in self._terms:
-            for var, pd in term.get_pds().items():
-                pd += ret.get(var, 0)
-                ret[var] = pd
-        return ret
+    def _diff(self, var):
+        return add(*[x._diff(var) for x in self._terms])
 
     def __str__(self):
         return self._str_infix_helper("+")
@@ -166,17 +191,13 @@ class _Sub(Expr):
     def __init__(self, f, g):
         self._f = f
         self._g = g
-        super(_Sub, self).__init__(f, g)
+        super().__init__(f, g)
 
     def get_value(self):
         return self._f.get_value() - self._g.get_value()
 
-    def get_pds(self):
-        ret = self._f.get_pds()
-        for var, pd in self._g.get_pds().items():
-            pd = ret.get(var, 0) - pd
-            ret[var] = pd
-        return ret
+    def _diff(self, var):
+        return self._f._diff(var) - self._g._diff(var)
 
     def __str__(self):
         return self._str_infix_helper("-")
@@ -184,21 +205,21 @@ class _Sub(Expr):
 
 class _Mul(Expr):
     def __init__(self, *terms):
-        super(_Mul, self).__init__(*terms)
+        super().__init__(*terms)
 
     def get_value(self):
-        return _multiply(self._term_values())
+        return functools.reduce(operator.mul, [term.get_value() for term in self._terms])
 
-    def get_pds(self):
-        pds_list, all_vars = self._pds_helper()
-        term_values = list(self._term_values())
-        value = _multiply(term_values)
+    def _diff(self, var):
+        values = [term.get_value() for term in self._terms]
 
-        return {
-            var: math.fsum(value * pds_list[i][var] / term_values[i]
-                for i in range(len(self._terms))
-                if var in pds_list[i] and term_values[i] != 0)
-            for var in all_vars}
+        tmp = []
+
+        for i, term in enumerate(self._terms):
+            tmp.append(mul(term._diff(var),
+                       *[self._terms[j] for j in range(len(self._terms)) if i != j]))
+
+        return add(*tmp)
 
     def __str__(self):
         return self._str_infix_helper("*")
@@ -208,20 +229,15 @@ class _Div(Expr):
     def __init__(self, f, g):
         self._f = f
         self._g = g
-        super(_Div, self).__init__(f, g)
+        super().__init__(f, g)
 
     def get_value(self):
         return self._f.get_value() / self._g.get_value()
 
-    def get_pds(self):
-        (f_pds, g_pds), all_vars = self._pds_helper()
-        f_val, g_val = self._term_values()
-
-        tmp = 1 / (g_val * g_val)
-
-        return {
-            var: tmp * (f_pds.get(var, 0) * g_val - g_pds.get(var, 0) * f_val)
-            for var in all_vars}
+    def _diff(self, var):
+        g_squared = self._g * self._g
+        return (self._f._diff(var) * self._g -
+                self._f * self._g._diff(var)) / ( self._g * self._g)
 
     def __str__(self):
         return self._str_infix_helper("/")
@@ -231,45 +247,42 @@ class _Pow(Expr):
     def __init__(self, f, power):
         self._f = f
         self._power = power
+        super().__init__(f)
 
     def get_value(self):
         return self._f.get_value() ** self._power
 
-    def get_pds(self):
-        tmp = (self._power) * self._f.get_value() ** (self._power - 1)
-        return {var: tmp * pd for var, pd in self._f.get_pds().items()}
+    def _diff(self, var):
+        return _Constant(self._power) * pow(self._f, self._power - 1) * self._f._diff(var)
 
     def __str__(self):
         return "(" + str(self._f) + "^" + str(self._power) + ")"
 
-class _Sq(Expr):
+class _Sq(_Pow):
     def __init__(self, f):
-        self._f = f
-        super(_Sq, self).__init__(f)
+        super().__init__(f, 2)
 
     def get_value(self):
         val = self._f.get_value()
         return val * val
 
-    def get_pds(self):
-        val = self._f.get_value()
-        return {var: 2 * val * pd for var, pd in self._f.get_pds().items()}
+    def _diff(self, var):
+        return _Constant(2) * self._f * self._f._diff(var)
 
     def __str__(self):
         return self._str_func_helper("sq")
 
 
-class _Sqrt(Expr):
+class _Sqrt(_Pow):
     def __init__(self, f):
         self._f = f
-        super(_Sqrt, self).__init__(f)
+        super().__init__(f, 1/2)
 
     def get_value(self):
         return math.sqrt(self._f.get_value())
 
-    def get_pds(self):
-        tmp = 1 / (2 * math.sqrt(self._f.get_value()))
-        return {var: tmp * pd for var, pd in self._f.get_pds().items() if pd != 0}
+    def _diff(self, var):
+        return _Constant(1/2) * self._f._diff(var) / sqrt(self._f)
 
     def __str__(self):
         return self._str_func_helper("sqrt")
@@ -283,42 +296,16 @@ class _Acos(Expr):
     def get_value(self):
         return math.acos(self._f.get_value())
 
-    def get_pds(self):
-        f_value = self._f.get_value()
-        tmp = -1 / math.sqrt(1 - f_value * f_value)
-        return {var: tmp * pd for var, pd in self._f.get_pds().items() if pd != 0}
+    def _diff(self, var):
+        return -self._f._diff(var) / sqrt(1 - sq(self._f))
 
     def __str__(self):
         return self._str_func_helper("acos")
 
 
-#class DotProduct(Expr):
-#    def __init__(self, *terms):
-#        """ Takes exactly 8 expressions, ordered like this: [x, y], [p1, p2]. [line1, line2];
-#        (line1.p1.x, line1.p1.y, line1.p2.x, ..., line2.p2.y)."""
-#        if len(terms) != 8:
-#            raise ValueError("Exactly 8 terms objects must be provided")
-#
-#        self._terms = terms
-#
-#    def get_value(self):
-#        terms = [var.get_value() for var in self._terms]
-#        ux = terms[0] - terms[2]
-#        uy = terms[1] - terms[3]
-#        vx = terms[4] - terms[6]
-#        vy = terms[5] - terms[7]
-#
-#        return ux * vx + uy * vy
-#
-#    def get_pds(self):
-#        val = [var.get_value() for var in self._terms]
-#        pds = [var.get_pds() for var in self._terms]
-#        terms = set.union(*[x.keys() for x in pds])
-#
-#        return {
-#            (pds[0].get(var, 0) - pds[2].get(var, 0)) * (val[4] - val[6]) +
-#            (val[0] - val[2]) * (pds[4].get(var, 0) - pds[6].get(var, 0)) +
-#            (pds[1].get(var, 0) - pds[3].get(var, 0)) * (val[5] - val[7]) +
-#            (val[1] - val[3]) * (pds[5].get(var, 0) - pds[7].get(var, 0))
-#            for var in terms}
-#
+def _wrap_consts(*args):
+    return [_wrap_const(x) for x in args]
+
+def _wrap_const(expr):
+    return expr if isinstance(expr, Expr) else _Constant(expr)
+
--- tests/expr_tests.py
+++ tests/expr_tests.py
@@ -9,7 +9,7 @@ def test1():
 
     nose.tools.assert_equals(expr.get_value(), 8)
 
-    pds = expr.get_pds()
+    pds = e.numeric_diffs(expr)
     nose.tools.assert_equals(set(pds.keys()), {x, y})
     nose.tools.assert_equals(pds[x], 1)
     nose.tools.assert_equals(pds[y], -1)
@@ -24,9 +24,9 @@ def check_pds(expr, variables):
             print(str(var2) + " = " + str(val))
             var2.set_value(val)
 
-        pds = pd = expr.get_pds()
 
-        pd = pds[var]
+        pd = e.diff(expr, var)
+        pd_value = pd.get_value()
 
         var.update_value(-epsilon)
         value1 = expr.get_value()
@@ -34,11 +34,11 @@ def check_pds(expr, variables):
         value2 = expr.get_value()
 
         numeric_pd = (value2 - value1) / (2 * epsilon)
-        print("(d {}) / (d {}) = ({} - {}) / (2 * {}) = {} (expected {})".format(
+        print("(d {}) / (d {}) = ({} - {}) / (2 * {}) = {} (expected {} = {})".format(
             str(expr), str(var),
-            value2, value1, epsilon, numeric_pd, pd))
+            value2, value1, epsilon, numeric_pd, pd, pd_value))
 
-        nose.tools.assert_almost_equals(pd, numeric_pd, delta=epsilon)
+        nose.tools.assert_almost_equals(numeric_pd, pd_value, delta=epsilon)
 
 def pds_test():
     x = e.Variable(5, "x")
@@ -46,12 +46,12 @@ def pds_test():
     z = e.Variable(5, "z")
 
     check_pds(x, {x: 10})
-    check_pds(e.add(x, y, z), {x: 0.6, y: 10, z: 3})
-    check_pds(e.sub(x, y), {x: 0.6, y: 10})
-    check_pds(e.mul(x, y, z), {x: 0.6, y: 10, z: 3})
-    check_pds(e.div(x, y), {x: 0.6, y: 10})
+    check_pds(x + y + z, {x: 0.6, y: 10, z: 3})
+    check_pds(x - y, {x: 0.6, y: 10})
+    check_pds(x * y * z, {x: 0.6, y: 10, z: 3})
+    check_pds(x / y, {x: 0.6, y: 10})
     check_pds(e.sq(x), {x: 0.6})
-    check_pds(e.neg(x), {x: 0.6})
+    check_pds(-x, {x: 0.6})
     check_pds(e.sqrt(x), {x: 0.6})
     check_pds(e.pow(x, 2), {x: 0.6})
     check_pds(e.pow(x, 5), {x: 0.6})
