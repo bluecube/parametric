321de733f0bb4ab6fe6454fda8f282ce599451a2
Kuba Marek <blue.cube@seznam.cz>
2018-07-21 23:32:15 +0200

    1D numpy-like dynamic array

---
+++ parametric/util.py
@@ -0,0 +1,120 @@
+import numpy
+
+import operator
+
+class DynamicArray:
+    """ Dynamic 1D array that supports efficient appends (like array.array) based on numpy (supporting dtypes) """
+
+    _initial_size = 10
+
+    def __init__(self, dtype=None, size_hint=None):
+        dt = numpy.dtype(dtype)
+        if dt.shape != ():
+            raise ValueError("Sub-aray dtypes are not supported now")
+
+        initial_size = self._initial_size if size_hint is None else size_hint
+        self._array = numpy.zeros((initial_size,), dtype=dtype)
+        self.size = 0
+
+    def _update_size(self, size, min_size=0):
+        self._array.resize(max(3 * size // 2, min_size))
+
+    def _maybe_inflate(self, size):
+        if size > len(self._array):
+            self._update_size(size)
+
+    def _maybe_deflate(self, size):
+        if size < len(self._array) / 2:
+            self._update_size(size, self._initial_size)
+
+    def append(self, value):
+        new_size = self.size + 1
+        self._maybe_inflate(new_size)
+        self._array[self.size] = value
+        self.size = new_size
+
+    def pop(self):
+        if self.size == 0:
+            raise IndexError("Pop from empty array")
+
+        v = self._array[self.size - 1]
+
+        self.size -= 1
+        self._maybe_deflate(self.size)
+
+    def extend(self, values):
+        offset = self.size
+
+        try:
+            length = len(values)
+        except TypeError:
+            pass
+        else:
+            new_size = self.size + length
+            self._maybe_inflate(new_size)
+            self.size = new_size
+
+            try:
+                # First try to do numpy slice assignment for speeed
+                self._array[offset:self.size] = values
+            except TypeError:
+                pass
+            else:
+                return
+
+            #TODO: Handle objects that report different length than it actualy is
+            # (list does that). At that point length and size_hint are basically equivalent
+            for i, value in enumerate(values, offset):
+                self._array[i] = value
+            return
+
+        #TODO: Use size_hint
+        for value in values:
+            self.append(value)
+
+    def pop_n(self, n):
+        if n > self.size:
+            raise IndexError("Not enough values to pop")
+        self.size -= n
+        self._maybe_deflate(self.size)
+
+    def reserve(self, size):
+        """ Resize the internal array to size if possible.
+        Useful to pre allocate area for later insertions in advance.
+        Note that this is one shot only, any following operation will cause the
+        the array to resize as normal. """
+        self._array.resize(max(self.size, size))
+
+    def shrink(self):
+        """ Shrink the internal array as much as possible.
+        Equivalent to reserve(0) """
+        self._array.resize(self.size)
+
+    def array(self):
+        """ Return a slice of the internal numpy array """
+        return self._array[:self.size]
+
+    def __array__(self, dtype=None):
+        """ To match numpy's protocol """
+        if dtype is None:
+            return self.array()
+        else:
+            return self.array(self.array(), dtype=dtype)
+
+    def __getitem__(self, key):
+        return self.array()[key]
+
+    def __setitem__(self, key, value):
+        self.array()[key] = value
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return iter(self.array())
+
+    def __str__(self):
+        return str(self.array())
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, str(self.array()))
---
+++ tests/test_dynamic_array.py
@@ -0,0 +1,105 @@
+import parametric.util
+
+import numpy
+import pytest
+
+
+@pytest.mark.parametrize("dtype", [None, numpy.int8, bytes, [('big', '>i4'), ('little', '<i4')], int])
+@pytest.mark.parametrize("size_hint", [None, 0, 1, 10, 100])
+def test_construction_empty(dtype, size_hint):
+    da = parametric.util.DynamicArray(dtype=dtype, size_hint=size_hint)
+
+    assert len(da) == 0
+    with pytest.raises(IndexError):
+        da[1]
+    if size_hint:
+        assert len(da._array) == size_hint
+
+
+def test_append():
+    da = parametric.util.DynamicArray(dtype=int, size_hint=10)
+
+    for i in range(100):
+        da.append(97*i % 100)
+
+    assert len(da) == 100
+    assert len(da._array) >= 100
+
+
+class Weirdo:
+    def __len__(self):
+        return 5
+    def __iter__(self):
+        return iter(range(5))
+
+@pytest.mark.parametrize("value, dtype", [([1, 2, 3], int),
+                                          (range(5), int),
+                                          (numpy.arange(8), float),
+                                          (numpy.arange(8), complex),
+                                          (Weirdo(), int)])
+@pytest.mark.parametrize("size_hint", [None, 0, 1, 100])
+def test_extend(value, dtype, size_hint):
+    da = parametric.util.DynamicArray(dtype=dtype, size_hint=size_hint)
+
+    da.extend(value)
+    assert list(da) == list(value)
+    da.extend(value)
+    assert list(da) == list(value) + list(value)
+
+
+def test_extend_iterator():
+    da = parametric.util.DynamicArray(dtype=int)
+    da.extend(iter(range(5)))
+    assert list(da) == list(range(5))
+
+@pytest.fixture
+def array():
+    da = parametric.util.DynamicArray(dtype=int)
+    da.extend(range(100))
+    return da
+
+def test_pop(array):
+    l = len(array)
+    array.pop()
+
+    assert len(array) == l - 1
+    assert list(array) == list(range(l - 1))
+
+def test_pop_all(array):
+    while len(array):
+        array.pop()
+
+    assert 100 > len(array._array) > 0
+
+    with pytest.raises(IndexError):
+        array.pop()
+
+def test_pop_n(array):
+    n = 50
+    l = len(array)
+    array.pop_n(n)
+
+    assert len(array) == l - n
+    assert list(array) == list(range(l - n))
+    assert 100 > len(array._array) > 0
+
+def test_indexing_single(array):
+    for i in range(len(array)):
+        assert array[i] == i
+
+def test_indexing_slices(array):
+    for i in range(5):
+        offset = i * 20
+        for j in range(5):
+            count = 3 * j + 1
+
+            assert list(array[offset:offset + count]) == list(range(offset, offset + count))
+
+def test_record_dtype():
+    array = parametric.util.DynamicArray(dtype=[("a", numpy.int32), ("b", numpy.float32)])
+    for i in range(100):
+        array.append((i, i + 0.5))
+
+
+    assert list(array["a"]) == list(range(100))
+    assert list(array["b"] - 0.5) == list(range(100))
